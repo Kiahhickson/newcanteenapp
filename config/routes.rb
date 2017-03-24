370016ec201871c548fe60d57fa1db1b1595152f
Kiah Hickson <kiah@kiahhickson.com.au>
2017-03-24 03:14:05 +0000

    new changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
-  resources :categories
-  get 'admin' => 'admin#index'
-  controller :sessions do
-    get 'login' => :new
-    post 'login' => :create
-    delete 'logout' => :destroy
-  end
+  get 'admin/index'
+
+  get 'sessions/new'
 
   get 'sessions/create'
+
   get 'sessions/destroy'
+
   resources :users
+  resources :orders
   resources :line_items
   resources :carts
   
   get 'store/index'
+  resources :categories
+  
   resources :products do
     get :who_bought, on: :member
   end
+    
+  get "/pages/:page" => "pages#show"
+ 
+  root 'store#index', as: 'store'
   
-  
-  root 'categories#index', as: 'store'
+
 end
